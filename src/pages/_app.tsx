e45f9f9ad9922ee26c723525210383ee6a837bec
azazelm3dj3d <56496067+azazelm3dj3d@users.noreply.github.com>
2023-07-13 18:00:52 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
 import { type AppType } from "next/dist/shared/lib/utils"
-import PlausibleProvider from "next-plausible"
+// import PlausibleProvider from "next-plausible"
 
 import "../styles/globals.css"
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <PlausibleProvider domain="azazelm3dj3d.com">
-      <Component {...pageProps} />
-    </PlausibleProvider>
+    // <PlausibleProvider domain="azazelm3dj3d.com">
+    <Component {...pageProps} />
+    // </PlausibleProvider>
   )
 }
 
